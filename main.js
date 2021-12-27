5a789fdc41e7845dac348d7a4d96993b33b7719f
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-12-27 10:43:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,17 +21,17 @@ function gotResults(error, results) {
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
 
-    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
-    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").innerHTML = 'Som detectado de  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Cachorro detectado - '+dog+ ' Gato detectado - '+cat;
     document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
     document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 
     img = document.getElementById('animal_image');
 
-    if (results[0].label == "Barking") {
+    if (results[0].label == "Latido") {
       img.src = 'bark.gif';
       dog = dog+1;
-    } else if (results[0].label == "Meowing") {
+    } else if (results[0].label == "Miado") {
       img.src = 'meow.gif';
       cat = cat + 1;
     } else{
